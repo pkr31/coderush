90aaffc188eddc48940ef5c1a3e77161c5beb7c2
Gurpinderpal Singh <gurpinderpalsingh88@gmail.com>
2019-09-22 19:45:34 +0530

    Commit for UserRole page

--- coderush/src/app/app-routing.module.ts
+++ coderush/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user and role/user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/UserRole/Role', pathMatch: 'full'},
+  { path: 'UserRole/Role', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- coderush/src/app/app.module.ts
+++ coderush/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { PageService, SortService, FilterService, GroupService, GridModule } from '@syncfusion/ej2-angular-grids';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user and role/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GridModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PageService, SortService, FilterService, GroupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
